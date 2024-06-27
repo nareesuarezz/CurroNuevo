c8ec08ecccd33ff73e4d545b632e5b93f67d1b17
nareesuarezz <naremehs11@gmail.com>
2024-06-27 11:01:25 +0100

    Fecha de validez working

--- curro/curro/src/App.js
+++ curro/curro/src/App.js
@@ -35,28 +35,35 @@ function App() {
 
     // Convertir el texto a mayúsculas y eliminar caracteres no alfanuméricos
     const cleanedText = text.toUpperCase().replace(/[^A-Z0-9 ]/g, '');
-    
+
     const words = cleanedText.split(' ');
-    
+
     const dniIndex = words.indexOf('DNI');
     const dni = dniIndex !== -1 ? words[dniIndex + 1] : 'No encontrado';
-    
+
     const apellidosIndex = words.indexOf('APELLIDOS');
     const apellidos = apellidosIndex !== -1 ? words[apellidosIndex + 1] + ' ' + words[apellidosIndex + 2] : 'No encontrado';
-    
+
     const nombreIndex = words.indexOf('NOMBRE');
     const nombre = nombreIndex !== -1 ? words[nombreIndex + 2] : 'No encontrado';
-    
-    setDni(`DNI: ${dni}, Apellidos: ${apellidos}, Nombre: ${nombre}`);
-    console.log(apellidos);
-  }    
+
+    const validezIndex = words.indexOf('VALIDEZY');
+    const validez = validezIndex !== -1 ? words[validezIndex + 5] + ' ' + words[validezIndex + 6] + ' ' + words[validezIndex + 7] : 'No encontrado';
+
+    setDni(`DNI: ${dni}\nApellidos: ${apellidos}\nNombre: ${nombre}\nValidez: ${validez}`);
+    console.log('Texto limpio:', cleanedText);
+    console.log('Índice de DNI:', dniIndex);
+    console.log('Índice de APELLIDOS:', apellidosIndex);
+    console.log('Índice de NOMBRE:', nombreIndex);
+    console.log('Índice de VALIDEZ:', validezIndex);
+  }
 
   return (
     <div>
       {/* {console.log(dni)} */}
       <input type="file" onChange={handleImageUpload} />
       {preview && <img src={preview} alt="Preview" style={{ width: '400px', height: '400px' }} />}
-      {dni && <p>{dni}</p>}
+      {dni && <pre>{dni}</pre>}
       {error && <p style={{ color: 'red' }}>{error}</p>}
 
       <button onClick={createFile}>Descargar archivo</button>
